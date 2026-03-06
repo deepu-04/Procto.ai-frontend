82c77d9accc6e0c0612301d9c0e6977302305a3e
deepu-04 <22pa1a12e7@vishnu.edu.in>
2026-03-06 22:48:37 +0530

    initial commit

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,33 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://procto-ai-backend.onrender.com', // NO /api
+  baseURL: 'https://procto-ai-backend.onrender.com',
   withCredentials: true,
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    let token = localStorage.getItem('token');
+    
+    if (!token) {
+      const userInfoStr = localStorage.getItem('userInfo');
+      if (userInfoStr) {
+        try {
+          const userInfo = JSON.parse(userInfoStr);
+          token = userInfo.token;
+        } catch (e) {}
+      }
+    }
+    
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
